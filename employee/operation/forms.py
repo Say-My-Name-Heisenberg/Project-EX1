cbdc6983df42169fa6cf52690d40e544585ae45c
Say-My-Name-Heisenberg <122552247+Say-My-Name-Heisenberg@users.noreply.github.com>
2023-01-23 22:18:28 +0530

    .

--- employee/operation/forms.py
+++ employee/operation/forms.py
@@ -3,3 +3,13 @@ from django import forms
 class OpForm (forms.Form):
     n1 = forms.IntegerField(label="Enter first Number")
     n2 = forms.IntegerField(label="Enter Second Number")
+    def clean(self):
+        cleaned_data=super().clean()
+        n1=cleaned_data.get("num1")
+        n2=cleaned_data.get("num2")
+        if n1<0:
+            msg="input value less than zero"
+            self.add_error("num1",msg)
+        if n2<0:
+            msg="input value less than zero"
+            self.add_error("num2",msg)
