7fa68b1c0a1a932076372230949a835d392adedf
Say-My-Name-Heisenberg <122552247+Say-My-Name-Heisenberg@users.noreply.github.com>
2023-01-23 23:31:16 +0530

    ,

--- employee/operation/forms.py
+++ employee/operation/forms.py
@@ -7,9 +7,9 @@ class OpForm (forms.Form):
         cleaned_data=super().clean()
         n1=cleaned_data.get("num1")
         n2=cleaned_data.get("num2")
-        if n1<0:
+        if n1 < 0:
             msg="input value less than zero"
             self.add_error("num1",msg)
-        if n2<0:
+        if n2 < 0:
             msg="input value less than zero"
             self.add_error("num2",msg)
