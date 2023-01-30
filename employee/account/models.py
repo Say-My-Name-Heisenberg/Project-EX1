f55715ea599310eb300d0d48113111d7aee727a0
Say-My-Name-Heisenberg <jessepinkman9807@gmail.com>
2023-01-30 18:25:31 +0530

    db

--- employee/account/models.py
+++ employee/account/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Staff(models.Model):
+    first=models.CharField(max_length=100)
+    last=models.CharField(max_length=100)
+    mail=models.EmailField()
+    exp=models.IntegerField()
+    username=models.CharField(max_length=100)
+    password=models.CharField(max_length=100)
+    
